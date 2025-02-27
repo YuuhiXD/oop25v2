3a66dc431bfdffca3038bfc6d5d9bf2f7d546897
YuuhiXD <kusek-kuba@o2.pl>
2025-02-27 16:51:19 +0100

    aktualizacja Main

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -4,46 +4,44 @@ public class Main {
         Point p = new Point();
         p.x = 5.0;
         p.y = 3.0;
-        System.out.println("("+p.x+","+p.y+")");
+        System.out.println("(" + p.x + "," + p.y + ")");
 
-        System.out.println("P : "+p.toString());
+        System.out.println("P : " + p.toString());
 
         System.out.println(p.toSvg());
 
-        p.translate(5,-5);
-        System.out.println("translate: "+p);
+        p.translate(5, -5);
+        System.out.println("translate: " + p);
 
-        System.out.println("translated: "+p.translated(3,3));
-        Point translated = p.translated(3,3);
-        System.out.println("translated 2: "+translated);
+        System.out.println("translated: " + p.translated(3, 3));
+        Point translated = p.translated(3, 3);
+        System.out.println("translated 2: " + translated);
 
         Segment seg = new Segment();
 
         seg.a = new Point();
         seg.b = new Point();
 
-        seg.a.x=1.0;
-        seg.a.y=1.0;
-        seg.b.x=3;
-        seg.b.y=4;
+        seg.a.x = 1.0;
+        seg.a.y = 1.0;
+        seg.b.x = 3;
+        seg.b.y = 4;
 
-        System.out.println("Seg len: "+seg.length());
+        System.out.println("Seg len: " + seg.length());
 
+        Segment[] tab = new Segment[2];
+        tab[0] = seg;
+        tab[1] = new Segment();
+        tab[1].a = new Point();
+        tab[1].b = new Point();
+        tab[1].a.x = 0;
+        tab[1].a.y = 0;
+        tab[1].b.x = 12;
+        tab[1].b.y = 5;
 
+        Segment longest = Segment.maxSegment(tab);
+        System.out.println("longest: " + longest.length());
 
-    }
-    public static Segment maxSegment(Segment[] arr)
-    {
-        if (arr.length==0) return null;
-
-        Segment max = arr[0];
-        for(int i=1;i<arr.length;i++)
-        {
-            if(arr[i].length() > max.length())
-            {
-                max=arr[i];
-            }
-        }
-        return max;
     }
 }
+
