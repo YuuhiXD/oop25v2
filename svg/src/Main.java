0762f585a5a5e1409025487a4c0bdcf54684564d
YuuhiXD <kusek-kuba@o2.pl>
2025-02-27 16:06:14 +0100

    dodano Point

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello world");
+        Point p = new Point();
+        p.x = 5.0;
+        p.y = 3.0;
+        System.out.println("("+p.x+","+p.y+")");
+        System.out.println("P : "+p.toString());
     }
 }
