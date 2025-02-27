89aecc878ff37647790ddf762a525888023ee2be
YuuhiXD <kusek-kuba@o2.pl>
2025-02-27 16:19:25 +0100

    translating

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -5,6 +5,16 @@ public class Main {
         p.x = 5.0;
         p.y = 3.0;
         System.out.println("("+p.x+","+p.y+")");
+
         System.out.println("P : "+p.toString());
+
+        System.out.println(p.toSvg());
+
+        p.translate(5,-5);
+        System.out.println("translate: "+p);
+
+        System.out.println("translated: "+p.translated(3,3));
+        Point translated = p.translated(3,3);
+        System.out.println("translated 2: "+translated);
     }
 }
