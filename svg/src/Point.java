773e6beb165f2a9ca294fb2a531c163cb9ad3be6
YuuhiXD <kusek-kuba@o2.pl>
2025-02-27 16:37:36 +0100

    dodano segment

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -22,4 +22,3 @@ public class Point {
         return newPoint;
     }
 }
-
---
+++ svg/src/Segment.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.pow;
+
+public class Segment {
+    public Point a;
+    public Point b;
+
+    public double length()
+    {
+        return Math.sqrt(pow(a.x-b.x,2) + pow(a.y-b.y,2));
+    }
+}
