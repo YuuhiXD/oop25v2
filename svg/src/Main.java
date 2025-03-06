db318c61a4ebd08d3b24e5e56c0ba6bf1f5f7320
YuuhiXD <kusek-kuba@o2.pl>
2025-03-06 15:31:36 +0100

    Point ustawiony na private

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,48 +1,52 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        Point p = new Point();
-        p.x = 5.0;
-        p.y = 3.0;
-        System.out.println("(" + p.x + "," + p.y + ")");
-
-        System.out.println("P : " + p.toString());
-
-        System.out.println(p.toSvg());
-
-        p.translate(5, -5);
-        System.out.println("translate: " + p);
-
-        System.out.println("translated: " + p.translated(3, 3));
-        Point translated = p.translated(3, 3);
-        System.out.println("translated 2: " + translated);
-
-        Segment seg = new Segment();
-
-        seg.a = new Point();
-        seg.b = new Point();
-
-        seg.a.x = 1.0;
-        seg.a.y = 1.0;
-        seg.b.x = 3;
-        seg.b.y = 4;
-
-        System.out.println("Seg len: " + seg.length());
-
-        Segment[] tab = new Segment[2];
-        tab[0] = seg;
-        tab[1] = new Segment();
-        tab[1].a = new Point();
-        tab[1].b = new Point();
-        tab[1].a.x = 0;
-        tab[1].a.y = 0;
-        tab[1].b.x = 12;
-        tab[1].b.y = 5;
-
-        Segment longest = Segment.maxSegment(tab);
-        System.out.println("longest: " + longest.length());
-        System.out.println(seg.toSegmentString());
-        System.out.println(seg.toSegmentSvg());
+        Point p1 = new Point(30,40);
+        Point p2 = new Point();
+        p2.setY(40);
+        System.out.println(p1);
+        System.out.println(p2);
+//        p.x = 5.0;
+//        p.y = 3.0;
+//        System.out.println("(" + p.x + "," + p.y + ")");
+//
+//        System.out.println("P : " + p.toString());
+//
+//        System.out.println(p.toSvg());
+//
+//        p.translate(5, -5);
+//        System.out.println("translate: " + p);
+//
+//        System.out.println("translated: " + p.translated(3, 3));
+//        Point translated = p.translated(3, 3);
+//        System.out.println("translated 2: " + translated);
+//
+//        Segment seg = new Segment();
+//
+//        seg.a = new Point();
+//        seg.b = new Point();
+//
+//        seg.a.x = 1.0;
+//        seg.a.y = 1.0;
+//        seg.b.x = 3;
+//        seg.b.y = 4;
+//
+//        System.out.println("Seg len: " + seg.length());
+//
+//        Segment[] tab = new Segment[2];
+//        tab[0] = seg;
+//        tab[1] = new Segment();
+//        tab[1].a = new Point();
+//        tab[1].b = new Point();
+//        tab[1].a.x = 0;
+//        tab[1].a.y = 0;
+//        tab[1].b.x = 12;
+//        tab[1].b.y = 5;
+//
+//        Segment longest = Segment.maxSegment(tab);
+//        System.out.println("longest: " + longest.length());
+//        System.out.println(seg.toSegmentString());
+//        System.out.println(seg.toSegmentSvg());
     }
 }
 
--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -6,7 +6,7 @@ public class Segment {
 
     public double length()
     {
-        return Math.sqrt(pow(a.x-b.x,2) + pow(a.y-b.y,2));
+        return Math.sqrt(pow(a.getX()-b.getX(),2) + pow(a.getY() -b.getY(),2));
     }
     public static Segment maxSegment(Segment[] arr)
     {
@@ -23,11 +23,11 @@ public class Segment {
         return max;
     }
     public String toSegmentString(){
-        return "("+a.x+" , "+a.y+" , "+b.x+", " +b.y+")";
+        return "("+ a.getX()+" , "+ a.getY()+" , "+ b.getX()+", " + b.getY()+")";
     }
     public String toSegmentSvg()
     {
-        return "<rect x=\""+a.x+"\" y=\""+a.y+"\" rx=\""+b.x+"\" ry=\""+b.y+"\" width=\"150\" height=\"150\" style=\"fill:red;stroke:black;stroke-width:5;opacity:0.5\" />";
+        return "<rect x=\""+ a.getX()+"\" y=\""+ a.getY()+"\" rx=\""+ b.getX()+"\" ry=\""+ b.getY()+"\" width=\"150\" height=\"150\" style=\"fill:red;stroke:black;stroke-width:5;opacity:0.5\" />";
     }
 }
 
